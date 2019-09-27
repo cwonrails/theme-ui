80f6781d7008586709d7628d0edffba1b98409f1
Chris Watson <cwonrails@gmail.com>
2019-09-27 11:43:02 -0400

    add draft work for creating preset

---
+++ superstylin.js
@@ -0,0 +1,47 @@
+export const superstylin = {
+  initialColorMode: 'light',
+  colors: {
+    text: '',
+    background: '',
+    primary: '',
+    secondary: '',
+    accent: '',
+    muted: '',
+    modes: {
+      dark: {
+        text: '',
+        background: '',
+        primary: '',
+        secondary: '',
+        accent: '',
+        muted: '',
+      },
+    },
+  },
+  fonts: {
+    body: '',
+    heading: '',
+    monospace: '',
+  },
+  fontSizes: {},
+  fontWeights: {},
+  lineHeights: {
+    body: 1.563,
+    // dummy value to allow prettier to write
+    heading: 1.25,
+  },
+  space: {},
+  letterSpacings: {},
+  sizes: {},
+  borders: {},
+  borderWidths: {},
+  borderStyles: {},
+  radii: {},
+  shadows: {},
+  zIndices: {},
+  styles: {
+    h1: {},
+  },
+}
+
+export default superstylin
