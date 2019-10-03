28e0318730489702409a9bb1b6b854f2c73e9e78
Chris Watson <cwonrails@gmail.com>
2019-10-03 19:19:32 -0400

    add font sizes & font families to superstylin.js

--- packages/preset-superstylin/superstylin.js
+++ packages/preset-superstylin/superstylin.js
@@ -19,11 +19,22 @@ export const superstylin = {
     },
   },
   fonts: {
-    body: '',
-    heading: '',
-    monospace: '',
+    body:
+      '-apple-system, BlinkMacSystemFont, "avenir next", avenir, "helvetica neue", helvetica, ubuntu, roboto, noto, "segoe ui", arial, sans-serif',
+    heading: 'inherit',
+    monospace:
+      '"SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace',
   },
-  fontSizes: {},
+  fontSizes: [
+    '3.052rem',
+    '2.441rem',
+    '1.953rem',
+    '1.563rem',
+    '1.25rem',
+    '1rem',
+    '0.8rem',
+    '0.64rem',
+  ],
   fontWeights: {},
   lineHeights: {
     body: 1.563,
@@ -38,7 +49,6 @@ export const superstylin = {
   borderStyles: {},
   radii: {},
   shadows: {},
-  zIndices: {},
   styles: {
     h1: {},
   },
